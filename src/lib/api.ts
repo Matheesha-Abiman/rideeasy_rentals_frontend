c7f64eff79e5ce6b0019540cf167268d7b7b30a9
Matheesha-Abiman <matheeshaabi@gmail.com>
2025-12-08 14:30:26 +0530

    edit files

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api/v1";
+const API_BASE_URL = "https://rideeasy-rentals-backen-git-9f369b-matheeshas-projects-cdae3c16.vercel.app/api/v1";
 
 export interface AuthResponse {
   message: string;
